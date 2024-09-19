7bffcb6f1424b9c2ec57e1dd73fb60ae62ea9334
Ghalib <ghalibfaruqe@duck.com>
2024-09-20 02:20:27 +0600

    number to zero steps problem

---
+++ numberTozero.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Solution {
+    public:
+   int numberOfSteps(int num) {
+       int count=0;
+        
+        
+        while(num!=0){
+            if(num%2!=0){
+                num = num-1;
+
+            }
+            else{
+            num = num/2;
+            
+            }
+            count++;
+        
+        }
+           
+       return count;
+    }
+    
+};
+int main(){ 
+Solution obj;   
+
+cout<<obj.numberOfSteps(4);
+return 0;
+}   
