07142d2acb1fcfbd6d6febe07bff839ed9242d43
Ghalib <ghalibfaruqe@duck.com>
2024-09-21 23:57:20 +0600

    Comment added

--- numberTozero.cpp
+++ numberTozero.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
+///Leetcode problem 
 class Solution {
     public:
+    /**
+     * @brief Returns the number of steps required to reduce a given number to 0 by either subtracting 1 or dividing by 2.
+     * @details The function takes an integer as input and returns the number of steps required to reduce it to 0. The steps are either subtracting 1 if the number is odd or dividing by 2 if the number is even.
+
+     */
    int numberOfSteps(int num) {
        int count=0;
         
